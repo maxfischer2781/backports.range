aedfe204ba486b32fa28a1d3f00fa09f7158833e
Max Fischer <maxfischer2781@gmail.com>
2016-11-04 15:08:49 +0100

    fixed ll iterator pickling, using more descriptive attribute name

--- backports/range/cyrange_iterator.pyx
+++ backports/range/cyrange_iterator.pyx
@@ -3,10 +3,10 @@ from .pyrange_iterator import range_iterator
 cdef class llrange_iterator(object):
     cdef long long _start
     cdef long long _step
-    cdef long long _stop
+    cdef long long _max_idx
     cdef long long _current
 
-    def __init__(self, long long start, long long step, long long count):
+    def __init__(self, long long start, long long step, long long count, long long current=-1):
         """
         Iterator over a `range`, for internal use only
 
@@ -14,21 +14,21 @@ cdef class llrange_iterator(object):
         """
         self._start = start
         self._step = step
-        self._stop = count - 1
-        self._current = -1
+        self._max_idx = count - 1
+        self._current = current
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self._current == self._stop:
+        if self._current == self._max_idx:
             raise StopIteration
         self._current += 1
         return self._start + self._step * self._current
 
     def __length_hint__(self):
         # both stop and current are offset by 1 which cancels out here
-        return self._stop - self._current
+        return self._max_idx - self._current
 
     # Pickling
     def __reduce__(self):
@@ -38,4 +38,4 @@ cdef class llrange_iterator(object):
         # we use the plain python iterator because:
         # - the extension type is NOT a valid type for pickle (because of reasons?)
         # - the extension type may be unavailable
-        return range_iterator, (self._start, self._stop, self._step, self._current), None, None, None
+        return self.__class__, (self._start, self._step, self._max_idx + 1, self._current), None, None, None
